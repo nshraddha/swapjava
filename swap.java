96c1678efc188d0b05948612e1b12ade206a8e8d
nshraddha <shranag6789@gmail.com>
2013-03-12 11:47:34 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,30 @@
+public class swap{
+  
+	public static void main(String[] args){
+		
+		String fname = "Shraddha";
+		String lname = "Nagaraj";
+		
+		System.out.println("Sample swap app:");
+		System.out.println("Before swap : ");
+		System.out.println("First name: "+fname);
+		System.out.println("Last name: "+lname);
+		
+		swap(fname,lname);
+		
+	}
+
+	private static void swap(String fname, String lname) {
+		// TODO Auto-generated method stub
+		
+		String temp = "";
+		
+		temp = fname;
+		fname = lname;
+		lname = temp;
+		
+		System.out.println("After swap : ");
+		System.out.println("First name: "+fname);
+		System.out.println("Last name: "+lname);
+	}
+}
